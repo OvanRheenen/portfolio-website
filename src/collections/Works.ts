64099d92e726b9e7978dac18afc08a7670e94012
Oliver van Rheenen <ovanrheenen@gmail.com>
2026-06-03 01:01:36 +0200

    Update Works collection

--- src/collections/Works.ts
+++ src/collections/Works.ts
@@ -27,18 +27,34 @@ export const Works: CollectionConfig = {
 			defaultValue: false,
 		},
 		{
-			name: 'imageWithPunchhole',
+			name: 'previewImage',
 			type: 'upload',
 			relationTo: 'media',
 			required: true,
 		 	label: 'Image with Punchhole',
+			admin: {
+				description: 'This image will be used as a preview when its corresponding punchhole is hovered over, it should be an image with a punchhole cutout.',
+			},
 		},
 		{
-			name: 'punchholeCutout',
+			name: 'punchholeImage',
 			type: 'upload',
 			relationTo: 'media',
 			required: true,
 		 	label: 'Punchhole Cutout',
+			admin: {
+				description: 'This image will be used in the homepage overview as one of the spread around dots.',
+			},
+		},
+		{
+			name: 'projectImages',
+			type: 'upload',
+			relationTo: 'media',
+			hasMany: true,
+		 	label: 'Project Display Images',
+			admin: {
+				description: 'These images will be used in the project detail view, when a user clicks on a project dot.',
+			},
 		},
   ],
 };
