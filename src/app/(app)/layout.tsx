cebc8eee32284a54d0cdd940cc204abe26e16a68
Oliver van Rheenen <ovanrheenen@gmail.com>
2026-06-03 00:59:31 +0200

    Update Header and Footer out of main content in layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -32,11 +32,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+				<Header />
 				<main>
-					<Header />
 					{children}
-					<Footer />
 				</main>
+				<Footer />
 			</body>
     </html>
   );
