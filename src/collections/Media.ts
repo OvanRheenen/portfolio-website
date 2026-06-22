19e44a14096768acff71cea613d80fa1066669a9
Oliver van Rheenen <ovanrheenen@gmail.com>
2026-06-23 01:49:38 +0200

    Add mime types whitelist for Payload 'Media' collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,7 +2,16 @@ import { CollectionConfig } from 'payload';
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  upload: true,
+  upload: {
+    mimeTypes: [
+			'image/jpeg',
+			'image/png',
+			'image/webp',
+			'image/avif',
+			'video/mp4',
+			'video/webm'
+		],
+  },
   fields: [
     { 
 			name: 'altText',
